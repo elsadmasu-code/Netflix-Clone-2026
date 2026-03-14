f343e02440633d763d5827d13b1143408b58ba56
elsadmasu-code <elsadmasu@gmail.com>
2026-03-14 08:54:05 -0700

    banner

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
+import Banner from '../../Components/Banner/Banner';
 const Home = () => {
   return (
     <div>
       <div>
         <Header />
+        <Banner />
         <Footer />
       </div>
     </div>
