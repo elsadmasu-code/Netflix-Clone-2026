beaf7496955d819cc36bc8f36f67d80176b79e91
elsadmasu-code <elsadmasu@gmail.com>
2026-03-14 12:58:05 -0700

    rows

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
 import Banner from '../../Components/Banner/Banner';
+import RowList from '../../Components/Rows/RowList/RowList';
 const Home = () => {
   return (
     <div>
       <div>
         <Header />
         <Banner />
+        <RowList />
         <Footer />
       </div>
     </div>
