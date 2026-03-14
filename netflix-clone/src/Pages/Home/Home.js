dae85de64309dd2307663bb44cc25ed36895351f
elsadmasu-code <elsadmasu@gmail.com>
2026-03-13 23:54:10 -0700

    commit header and footer

---
+++ netflix-clone/src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../../Components/Footer/Footer';
+import Header from '../../Components/Header/Header';
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default Home;
